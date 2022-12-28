bb2c5283744e65b51519b081f042fdbc1cb246e7
MahmoudAdelbghany2 <100173563+MahmoudAdelbghany2@users.noreply.github.com>
2022-12-28 13:54:19 +0200

    Update post-search.py
    added comments and some optimizations

--- intern-scrapper/post-search.py
+++ intern-scrapper/post-search.py
@@ -2,52 +2,52 @@ import time
 from selenium.webdriver.chrome.options import Options
 from selenium import webdriver
 from bs4 import BeautifulSoup
-# define the global variables
-USERNAME = 'mahmoud-abdelghany-rageh'
-EMAIL = ''
-PASSWORD = ''
 
-# create a new Chrome browser
+# Define the global variables for the LinkedIn email, password, and username.
+USERNAME = 'mahmoud-abdelghany-rageh'
+EMAIL = ''  # Insert your LinkedIn email here.
+PASSWORD = ''  # Insert your LinkedIn password here.
+
+# Create a new Chrome browser with the "user-data-dir" argument specified.
+# This allows the script to use a pre-existing login session for LinkedIn.
 options = Options()
 options.add_argument("user-data-dir=C:\\Users\\Mahmoud Abdelghany\\AppData\\Local\\Google\\Chrome\\User Data")
 driver = webdriver.Chrome(options=options)
 
-#login for the first time only
-
-# navigate to the LinkedIn login page
-#driver.get('https://www.linkedin.com/login')
-
-# # locate the email and password input elements
+# Uncomment the following block of code if you want to login for the first time.
+# # Navigate to the LinkedIn login page.
+# driver.get('https://www.linkedin.com/login')
+#
+# # Locate the email and password input elements.
 # email_input = driver.find_element(by='id', value='username')
 # password_input = driver.find_element(by='id', value='password')
 #
-# # locate the login button
+# # Locate the login button.
 # login_button = driver.find_element(by='xpath', value='//button[@type="submit"]')
 #
-# # enter your email and password
 # email_input.send_keys(EMAIL)
 # password_input.send_keys(PASSWORD)
 #
-# # click the login button
+# # Click the login button.
 # login_button.click()
 
-# wait for 5 seconds
-#time.sleep(5)
-
+# Define the URL of the LinkedIn company page.
 company = 'https://www.linkedin.com/company/209215/'
-# navigate to the LinkedIn company posts
+
+# Navigate to the LinkedIn company page and wait for 5 seconds.
 driver.get(f'{company}/posts/?feedView=all')
 time.sleep(5)
-#scroll some posts
+
+# Scroll through the company's posts.
 for i in range(11):
     driver.execute_script('window.scrollBy(0, 500);')
-soup = BeautifulSoup(driver.page_source, 'lxml')
 
+# Use the BeautifulSoup library to parse the HTML of the page and locate the elements containing the company's posts.
+soup = BeautifulSoup(driver.page_source, 'lxml')
 posts = soup.find_all("div", {"class": "feed-shared-update-v2__description-wrapper"})
+
+# Iterate through the posts, searching for the keywords of interest. If any are found, print a message indicating the post number.
 for i in range(len(posts)):
     posts[i] = str(posts[i].find("span", {"class": "break-words"}))
-#print(posts[0])
-for post in posts:
-    if( post.find('internship') != -1 or post.find('intern') != -1 or post.find('منحة') != -1 or post.find('تدريب') != -1):
-        #company is a place holder
-        print(f"found intern at #company# post number {posts.index(post)}")
+    if "internship" in posts[i] or "intern" in posts[i] or "منحة" in posts[i] or "تدريب" in posts[i]:
+        print(f"Found intern opportunity at #company# in post number {i}")
